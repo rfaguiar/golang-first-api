cb1819c83695d7e0c4db73921ef88152dda56ebf
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-05-03 19:11:09 -0300

    Implements delete endpoint using id parameter.

--- main.go
+++ main.go
@@ -43,6 +43,7 @@ func main() {
 	router.HandleFunc("/api-v1/user", getUsers).Methods("GET")
 	router.HandleFunc("/api-v1/user/{id}", getAnUser).Methods("GET")
 	router.HandleFunc("/api-v1/user", createUser).Methods("POST")
+	router.HandleFunc("/api-v1/user/{id}", deleteUser).Methods("DELETE")
 	//show log server address
 	log.Print("Server listen http://localhost:9000")
 	//UP server and listen http port 9000 using default http multiplexer, if error log message and kill api server
@@ -66,6 +67,36 @@ func jsonMiddleware(handler http.Handler) http.Handler {
 		responseWriter.Header().Set("Content-Type", "application/json")
 		handler.ServeHTTP(responseWriter, request)
 	})
+}
+
+/*
+	Delete user by param id
+*/
+func deleteUser(responseWriter http.ResponseWriter, request *http.Request) {
+	params := mux.Vars(request)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		log.Print(err.Error())
+		responseWriter.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	log.Printf("DELETE /api-v1/user/%v", id)
+	//find user by id
+	index := -1
+	for key, u := range userRepo {
+		if u.Id == id {
+			index = key
+		}
+	}
+	if index < 0 {
+		responseWriter.WriteHeader(http.StatusNotFound)
+		return
+	}
+	//delete user in a repository
+	leftSlice := userRepo[0:index]
+	rightSlice := userRepo[index+1:]
+	userRepo = append(leftSlice, rightSlice...)
+
 }
 
 /*
