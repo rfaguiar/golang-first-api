d5cb8021854e12f08eeba6b6f00aec438cf3c8a4
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-05-02 21:56:04 -0300

    Implements endpoint user get an user when pass parameter id should find user in repository, implements global slice user repository for use in memory server and initialize, adjust json writer in a get endpoints.

--- main.go
+++ main.go
@@ -6,21 +6,15 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 /*
-	Startup server API
+	Type Health for use health checks endpoint
+	Ex: status: "UP"
 */
-func main() {
-	router := mux.NewRouter()
-	router.Use(jsonMiddleware)
-	router.HandleFunc("/", home).Methods("GET")
-	router.HandleFunc("/health", healthCheck).Methods("GET")
-	router.HandleFunc("/api-v1/user", getUsers).Methods("GET")
-	//show log server address
-	log.Print("Server listen http://localhost:9000")
-	//UP server and listen http port 9000 using default http multiplexer, if error log message and kill api server
-	log.Fatal(http.ListenAndServe(":9000", router))
+type Health struct {
+	Status string `json:"status"`
 }
 
 /*
@@ -33,25 +27,34 @@ type User struct {
 }
 
 /*
-	Show all users in a repository
+	Repository for users type
 */
-func getUsers(responseWriter http.ResponseWriter, _ *http.Request) {
-	log.Print("GET /user")
-	users := []User{
-		{Id: 1, Name: "Jorge", Age: 20},
-		{Id: 2, Name: "Jhon", Age: 33}}
-	js, err := json.Marshal(users)
-	if err != nil {
-		log.Print(err.Error())
-		responseWriter.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	_, err = responseWriter.Write(js)
-	if err != nil {
-		log.Print(err.Error())
-		responseWriter.WriteHeader(http.StatusInternalServerError)
-		return
-	}
+var userRepo []User
+
+/*
+	Startup server API
+*/
+func main() {
+	initializeDatabase()
+	router := mux.NewRouter()
+	router.Use(jsonMiddleware)
+	router.HandleFunc("/", home).Methods("GET")
+	router.HandleFunc("/health", healthCheck).Methods("GET")
+	router.HandleFunc("/api-v1/user", getUsers).Methods("GET")
+	router.HandleFunc("/api-v1/user/{id}", getAnUser).Methods("GET")
+	//show log server address
+	log.Print("Server listen http://localhost:9000")
+	//UP server and listen http port 9000 using default http multiplexer, if error log message and kill api server
+	log.Fatal(http.ListenAndServe(":9000", router))
+}
+
+/*
+	Initialize in memory database
+*/
+func initializeDatabase() {
+	userRepo = append(userRepo,
+		User{Id: 1, Name: "Jorge", Age: 20},
+		User{Id: 2, Name: "Jhon", Age: 33})
 }
 
 /*
@@ -65,11 +68,41 @@ func jsonMiddleware(handler http.Handler) http.Handler {
 }
 
 /*
-	Type Health for use health checks endpoint
-	Ex: status: "UP"
+	show an user
 */
-type Health struct {
-	Status string `json:"status"`
+func getAnUser(responseWriter http.ResponseWriter, request *http.Request) {
+	params := mux.Vars(request)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		log.Print(err.Error())
+		responseWriter.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	log.Printf("GET /api-v1/user/%v", id)
+	for _, user := range userRepo {
+		if user.Id == id {
+			err = json.NewEncoder(responseWriter).Encode(user)
+			if err != nil {
+				log.Print(err.Error())
+				responseWriter.WriteHeader(http.StatusInternalServerError)
+			}
+			return
+		}
+	}
+	responseWriter.WriteHeader(http.StatusNotFound)
+}
+
+/*
+	Show all users in a repository
+*/
+func getUsers(responseWriter http.ResponseWriter, _ *http.Request) {
+	log.Print("GET /api-v1/user")
+	err := json.NewEncoder(responseWriter).Encode(userRepo)
+	if err != nil {
+		log.Print(err.Error())
+		responseWriter.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 }
 
 /*
@@ -77,19 +110,13 @@ type Health struct {
 	if error print log and set status 500 Internal Server Error
 */
 func healthCheck(responseWriter http.ResponseWriter, _ *http.Request) {
-	log.Print("ALL /health")
-	h := Health{"UP"}
-	js, er := json.Marshal(h)
-	if er != nil {
-		log.Print(er.Error())
-		responseWriter.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	responseWriter.WriteHeader(http.StatusOK)
-	_, err := responseWriter.Write(js)
+	log.Print("GET /health")
+	health := Health{"UP"}
+	err := json.NewEncoder(responseWriter).Encode(health)
 	if err != nil {
 		log.Print(err.Error())
 		responseWriter.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 }
 
@@ -98,7 +125,7 @@ func healthCheck(responseWriter http.ResponseWriter, _ *http.Request) {
 */
 func home(responseWriter http.ResponseWriter, _ *http.Request) {
 	responseWriter.Header().Set("Content-Type", "text/plain")
-	log.Print("ALL / home")
+	log.Print("GET / home")
 	_, err := fmt.Fprint(responseWriter, "Server UP")
 	if err != nil { // if error then log error and return status code 500 Internal Server Error
 		log.Print(err.Error())
